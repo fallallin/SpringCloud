80a11ca78bbdace3594a7cbb94abc03370a0dc6a
afalla <falla.turnthepage@gmail.com>
2020-05-14 20:55:11 -0500

    Update ProductoClienteRest.java
    Ajuste de ortografia

--- springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/clientes/ProductoClienteRest.java
+++ springboot-servicio-item/src/main/java/com/formacionbdi/springboot/app/clientes/ProductoClienteRest.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.formacionbdi.springboot.app.item.models.Producto;
 
-//@FeignClient(name = "servicio-productos", url="localhost:8001") --> Con robbon ya no es necesario quemarlo
+//@FeignClient(name = "servicio-productos", url="localhost:8001") --> Con ribbon ya no es necesario quemarlo
 @FeignClient(name = "servicio-productos")
 public interface ProductoClienteRest {
 	
